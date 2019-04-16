8600782dd806ad3de04fe320d1463f87cb145912
Alexander Cherry <alex.cherry@oneglobeit.com>
2019-04-15 20:46:27 -0400

    new page, more examples

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,27 @@
 console.info('Initializing 508 compliance techincal demonstration.');
 
-const toggleContrast = (button) => {
+const toggleContrast = () => {
   document.getElementById("color-container").classList.toggle('bad-contrast');
 }
 
-const toggleContrast = (button) => {
-  document.getElementById("color-container").classList.toggle('bad-contrast');
+const toggleDeuteranomaly = () => {
+  document.getElementById("color-container").classList.remove('protanopia');
+  document.getElementById("color-container").classList.toggle('deuteranomaly');
+}
+
+const toggleProtanopia = () => {
+  document.getElementById("color-container").classList.remove('deuteranomaly');
+  document.getElementById("color-container").classList.toggle('protanopia');
 }
 
 const initializeEventHandlers = () => {
   document.body.addEventListener('click', (e) => {
     if (e.target.id === 'contrast-toggle') {
       toggleContrast();
-    } else if (e.target.id === 'grayscale-toggle') {
-      toggleGrayscale();
+    } else if (e.target.id === 'deuteranomaly-toggle') {
+      toggleDeuteranomaly();
+    } else if (e.target.id === 'protanopia-toggle') {
+      toggleProtanopia();
     }
   })
 }
